c1ae34bb6b7ed2c065118d5bbbb3f0e3af60f5f8
Sharky! <jdeurt@gmail.com>
2017-09-11 16:18:53 -0500

    Update Auton_Functions.c

--- Auton_Functions.c
+++ Auton_Functions.c
@@ -2,8 +2,11 @@
 
 void sMove(int speed, int amountOfPorts, S_PORTS port1, S_PORTS port2 = NONE, S_PORTS port3 = NONE, S_PORTS port4 = NONE, S_PORTS port5 = NONE) {
   switch(amountOfPorts) {
+
     default:
-      printf("Error: Incorrect syntax.");
+    
+    printf("Error: Incorrect syntax.");
+
     case 10:
 
     switch(port10) {
@@ -344,37 +347,5 @@ void sMove(int speed, int amountOfPorts, S_PORTS port1, S_PORTS port2 = NONE, S_
       motor[port10] = speed;
       break;
 
-  }
-  switch(port1) {
-    case PORT_1:
-      motor[port1] = speed;
-      break;
-    case PORT_2:
-      motor[port2] = speed;
-      break;
-    case PORT_3:
-      motor[port3] = speed;
-      break;
-    case PORT_4:
-      motor[port4] = speed;
-      break;
-    case PORT_5:
-      motor[port5] = speed;
-      break;
-    case PORT_6:
-      motor[port6] = speed;
-      break;
-    case PORT_7:
-      motor[port7] = speed;
-      break;
-    case PORT_8:
-      motor[port8] = speed;
-      break;
-    case PORT_9:
-      motor[port9] = speed;
-      break;
-    case PORT_10:
-      motor[port10] = speed;
-      break;
   }
 }
