da1439b34d67bdca321486c229a8657b615e4205
Sharky! <jdeurt@gmail.com>
2017-09-11 12:45:13 -0500

    Update Auton_Functions.c

--- Auton_Functions.c
+++ Auton_Functions.c
@@ -1,1 +1,6 @@
+//All custom functions will begin with a lowercase s
 
+void sMove(int time, int amountOfPorts, S_PORTS port1, S_PORTS port2, S_PORTS port3, S_PORTS port4, S_PORTS port5) {
+  for() {
+  }
+}
