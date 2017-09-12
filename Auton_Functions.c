e3d7d03d1bf7c235862a4aded2845eca27e0472e
jdeurt <jdeurt@gmail.com>
2017-09-11 19:49:11 -0500

    Updated Formtting

--- Auton_Functions.c
+++ Auton_Functions.c
@@ -1,361 +1,112 @@
 //All custom functions will begin with a lowercase s
+#include "Defines.c"
 
-void sMove(int speed, int amountOfPorts, S_PORTS port1, S_PORTS port2 = NONE, S_PORTS port3 = NONE, S_PORTS port4 = NONE, S_PORTS port5 = NONE) {
+void sMoveMin(int speed, int amountOfPorts, S_PORTS port1, S_PORTS port2, S_PORTS port3, S_PORTS port4, S_PORTS port5) {
   switch(amountOfPorts) {
 
     default:
     
-    printf("Error: Incorrect syntax.");
-
-    case 10:
-
-    switch(port10) {
-    case PORT_1:
-      motor[port1] = speed;
-      break;
-    case PORT_2:
-      motor[port2] = speed;
-      break;
-    case PORT_3:
-      motor[port3] = speed;
-      break;
-    case PORT_4:
-      motor[port4] = speed;
-      break;
-    case PORT_5:
-      motor[port5] = speed;
-      break;
-    case PORT_6:
-      motor[port6] = speed;
-      break;
-    case PORT_7:
-      motor[port7] = speed;
-      break;
-    case PORT_8:
-      motor[port8] = speed;
-      break;
-    case PORT_9:
-      motor[port9] = speed;
-      break;
-    case PORT_10:
-      motor[port10] = speed;
-      break;
-    }
-
-    case 9:
-
-    switch(port9) {
-    case PORT_1:
-      motor[port1] = speed;
-      break;
-    case PORT_2:
-      motor[port2] = speed;
-      break;
-    case PORT_3:
-      motor[port3] = speed;
-      break;
-    case PORT_4:
-      motor[port4] = speed;
-      break;
-    case PORT_5:
-      motor[port5] = speed;
-      break;
-    case PORT_6:
-      motor[port6] = speed;
-      break;
-    case PORT_7:
-      motor[port7] = speed;
-      break;
-    case PORT_8:
-      motor[port8] = speed;
-      break;
-    case PORT_9:
-      motor[port9] = speed;
-      break;
-    case PORT_10:
-      motor[port10] = speed;
-      break;
-    }
-
-    case 8:
-
-    switch(port8) {
-    case PORT_1:
-      motor[port1] = speed;
-      break;
-    case PORT_2:
-      motor[port2] = speed;
-      break;
-    case PORT_3:
-      motor[port3] = speed;
-      break;
-    case PORT_4:
-      motor[port4] = speed;
-      break;
-    case PORT_5:
-      motor[port5] = speed;
-      break;
-    case PORT_6:
-      motor[port6] = speed;
-      break;
-    case PORT_7:
-      motor[port7] = speed;
-      break;
-    case PORT_8:
-      motor[port8] = speed;
-      break;
-    case PORT_9:
-      motor[port9] = speed;
-      break;
-    case PORT_10:
-      motor[port10] = speed;
-      break;
-    }
-
-    case 7:
-
-    switch(port7) {
-    case PORT_1:
-      motor[port1] = speed;
-      break;
-    case PORT_2:
-      motor[port2] = speed;
-      break;
-    case PORT_3:
-      motor[port3] = speed;
-      break;
-    case PORT_4:
-      motor[port4] = speed;
-      break;
-    case PORT_5:
-      motor[port5] = speed;
-      break;
-    case PORT_6:
-      motor[port6] = speed;
-      break;
-    case PORT_7:
-      motor[port7] = speed;
-      break;
-    case PORT_8:
-      motor[port8] = speed;
-      break;
-    case PORT_9:
-      motor[port9] = speed;
-      break;
-    case PORT_10:
-      motor[port10] = speed;
-      break;
-    }
-
-    case 6:
-
-    switch(port6) {
-    case PORT_1:
-      motor[port1] = speed;
-      break;
-    case PORT_2:
-      motor[port2] = speed;
-      break;
-    case PORT_3:
-      motor[port3] = speed;
-      break;
-    case PORT_4:
-      motor[port4] = speed;
-      break;
-    case PORT_5:
-      motor[port5] = speed;
-      break;
-    case PORT_6:
-      motor[port6] = speed;
-      break;
-    case PORT_7:
-      motor[port7] = speed;
-      break;
-    case PORT_8:
-      motor[port8] = speed;
-      break;
-    case PORT_9:
-      motor[port9] = speed;
-      break;
-    case PORT_10:
-      motor[port10] = speed;
-      break;
-    }
+      printf("Error: Incorrect syntax.");
 
     case 5:
 
-    switch(port5) {
-    case PORT_1:
-      motor[port1] = speed;
-      break;
-    case PORT_2:
-      motor[port2] = speed;
-      break;
-    case PORT_3:
-      motor[port3] = speed;
-      break;
-    case PORT_4:
-      motor[port4] = speed;
-      break;
-    case PORT_5:
-      motor[port5] = speed;
-      break;
-    case PORT_6:
-      motor[port6] = speed;
-      break;
-    case PORT_7:
-      motor[port7] = speed;
-      break;
-    case PORT_8:
-      motor[port8] = speed;
-      break;
-    case PORT_9:
-      motor[port9] = speed;
-      break;
-    case PORT_10:
-      motor[port10] = speed;
-      break;
-    }
+      switch(port5) {
+        case PORT_1:
+          motor[port1] = speed;
+          break;
+        case PORT_2:
+          motor[port2] = speed;
+          break;
+        case PORT_3:
+          motor[port3] = speed;
+          break;
+        case PORT_4:
+          motor[port4] = speed;
+          break;
+        case PORT_5:
+          motor[port5] = speed;
+          break;
+      }
 
     case 4:
 
-    switch(port4) {
-    case PORT_1:
-      motor[port1] = speed;
-      break;
-    case PORT_2:
-      motor[port2] = speed;
-      break;
-    case PORT_3:
-      motor[port3] = speed;
-      break;
-    case PORT_4:
-      motor[port4] = speed;
-      break;
-    case PORT_5:
-      motor[port5] = speed;
-      break;
-    case PORT_6:
-      motor[port6] = speed;
-      break;
-    case PORT_7:
-      motor[port7] = speed;
-      break;
-    case PORT_8:
-      motor[port8] = speed;
-      break;
-    case PORT_9:
-      motor[port9] = speed;
-      break;
-    case PORT_10:
-      motor[port10] = speed;
-      break;
-    }
+      switch(port4) {
+        case PORT_1:
+          motor[port1] = speed;
+          break;
+        case PORT_2:
+          motor[port2] = speed;
+          break;
+        case PORT_3:
+          motor[port3] = speed;
+          break;
+        case PORT_4:
+          motor[port4] = speed;
+          break;
+        case PORT_5:
+          motor[port5] = speed;
+          break;
+      }
 
     case 3:
 
-    switch(port3) {
-    case PORT_1:
-      motor[port1] = speed;
-      break;
-    case PORT_2:
-      motor[port2] = speed;
-      break;
-    case PORT_3:
-      motor[port3] = speed;
-      break;
-    case PORT_4:
-      motor[port4] = speed;
-      break;
-    case PORT_5:
-      motor[port5] = speed;
-      break;
-    case PORT_6:
-      motor[port6] = speed;
-      break;
-    case PORT_7:
-      motor[port7] = speed;
-      break;
-    case PORT_8:
-      motor[port8] = speed;
-      break;
-    case PORT_9:
-      motor[port9] = speed;
-      break;
-    case PORT_10:
-      motor[port10] = speed;
-      break;
-    }
+      switch(port3) {
+        case PORT_1:
+          motor[port1] = speed;
+          break;
+        case PORT_2:
+          motor[port2] = speed;
+          break;
+        case PORT_3:
+          motor[port3] = speed;
+          break;
+        case PORT_4:
+          motor[port4] = speed;
+          break;
+        case PORT_5:
+          motor[port5] = speed;
+          break;
+      }
 
     case 2:
 
-    switch(port2) {
-    case PORT_1:
-      motor[port1] = speed;
-      break;
-    case PORT_2:
-      motor[port2] = speed;
-      break;
-    case PORT_3:
-      motor[port3] = speed;
-      break;
-    case PORT_4:
-      motor[port4] = speed;
-      break;
-    case PORT_5:
-      motor[port5] = speed;
-      break;
-    case PORT_6:
-      motor[port6] = speed;
-      break;
-    case PORT_7:
-      motor[port7] = speed;
-      break;
-    case PORT_8:
-      motor[port8] = speed;
-      break;
-    case PORT_9:
-      motor[port9] = speed;
-      break;
-    case PORT_10:
-      motor[port10] = speed;
-      break;
-    }
+      switch(port2) {
+        case PORT_1:
+          motor[port1] = speed;
+          break;
+        case PORT_2:
+          motor[port2] = speed;
+          break;
+        case PORT_3:
+          motor[port3] = speed;
+          break;
+        case PORT_4:
+          motor[port4] = speed;
+          break;
+        case PORT_5:
+          motor[port5] = speed;
+          break;
+      }
 
     case 1:
 
-    switch(port1) {
-    case PORT_1:
-      motor[port1] = speed;
-      break;
-    case PORT_2:
-      motor[port2] = speed;
-      break;
-    case PORT_3:
-      motor[port3] = speed;
-      break;
-    case PORT_4:
-      motor[port4] = speed;
-      break;
-    case PORT_5:
-      motor[port5] = speed;
-      break;
-    case PORT_6:
-      motor[port6] = speed;
-      break;
-    case PORT_7:
-      motor[port7] = speed;
-      break;
-    case PORT_8:
-      motor[port8] = speed;
-      break;
-    case PORT_9:
-      motor[port9] = speed;
-      break;
-    case PORT_10:
-      motor[port10] = speed;
-      break;
-    }
+      switch(port1) {
+        case PORT_1:
+          motor[port1] = speed;
+          break;
+        case PORT_2:
+          motor[port2] = speed;
+          break;
+        case PORT_3:
+          motor[port3] = speed;
+          break;
+        case PORT_4:
+          motor[port4] = speed;
+          break;
+        case PORT_5:
+          motor[port5] = speed;
+          break;
+      }
 
   }
 }
