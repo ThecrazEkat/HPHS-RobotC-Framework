189bc5826d355ca69b6ea9778a02e4afcb22dd94
jdeurt <jdeurt@gmail.com>
2017-09-11 21:34:06 -0500

    update

--- Defines.c
+++ Defines.c
@@ -15,24 +15,24 @@ typedef enum {
 typedef enum {
   NO_BUTTON = 0,
 
-  CH_1,   // 1
-  CH_2,   // 2
-  CH_3,   // 3
-  CH_4,   // 4
+  CH_1,
+  CH_2,
+  CH_3,
+  CH_4,
 
-  BTN_7U, // 5
-  BTN_7R, // 6
-  BTN_7D, // 7
-  BTN_7L, // 8
+  BTN_7U,
+  BTN_7R,
+  BTN_7D,
+  BTN_7L,
 
-  BTN_8U, // 9
-  BTN_8R, // 10
-  BTN_8D, // 11
-  BTN_8L, // 12
+  BTN_8U,
+  BTN_8R,
+  BTN_8D,
+  BTN_8L,
 
-  BTN_5U, // 13
-  BTN_5D, // 14
+  BTN_5U,
+  BTN_5D,
 
-  BTN_6U, // 15
-  BTN_6D  // 16
+  BTN_6U,
+  BTN_6D
 } S_CONTROLLER_BUTTON
